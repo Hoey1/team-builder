a5a58749f75be0ff3950bd30ebbd8d5aee14c1e5
Joey <joey@xm8.ninja>
2020-05-19 16:32:16 -0400

    MVP - I think

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,10 +1,30 @@
-import React from 'react'
-
-
+import React from "react";
 
 export default function Form(props) {
-    const {
-      name,
-      email,
-      role,
-    } = props
+  const { name, email, role } = props;
+
+  return (
+    <form className="form container" onSubmit={onSubmit}>
+      <div className="form-group submit">
+        <h2>Add a Friend</h2>
+        <button>submit</button>
+
+        <div className="form-group inputs">
+          <h4>Team information</h4>
+
+          <label>
+            Name:&nbsp;
+            <input
+              type="text"
+              placeholder="Type a Teammates Name"
+              maxLength="20"
+              name="teammate"
+              value={name.teammate}
+              // onChange={}
+            />
+          </label>
+        </div>
+      </div>
+    </form>
+  );
+}
